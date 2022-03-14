1c979723dde66366f76f78529267c87f9cfa0314
Christoph Mannich <cmannich@gmail.com>
2022-03-14 19:34:08 +0100

    comments and cleanup

--- sudokumain/sudokumain.py
+++ sudokumain/sudokumain.py
@@ -5,11 +5,6 @@ Created on Feb 20, 2022
 '''
 # Include the PySFML extension
 from sfml import sf
-from array import array
-#import array as arr
-from future.builtins import int
-from _operator import index
-from ast import Index
 
 focusIndex = -1
 font = sf.Font.from_file("/usr/share/fonts/truetype/hack/Hack-Regular.ttf")
@@ -24,6 +19,10 @@ font6 = sf.Font.from_file("/usr/share/fonts/truetype/liberation2/LiberationMono-
 
 
 
+# Each sudoku square contains an object defined by class box which contains all aspects and state of the square
+# Each sudoko contains 81 squares in an 9x9 grid. Each square has an index, 0 is top left and 80 is bottom right
+
+
 
 class box:
     def __init__(self, position: sf.Vector2, size: sf.Vector2, overlay = 0, background= 0 , value = " ", lock = 0):
@@ -35,6 +34,7 @@ class box:
         self.lock = lock
         self.possible = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+# Get the position and size of a square given it index
 def index2posSize(index: int):
     yposcomp = 0
     xposcomp = 0
@@ -60,33 +60,33 @@ def index2posSize(index: int):
 
     ysize = 74 + ysizecomp
     xsize = 74 + xsizecomp
-
-    #print("X:" + str(xpos) + " Y:" + str(ypos))
     return sf.Vector2(xpos,ypos), sf.Vector2(xsize,ysize)
 
-fyrkant = []
+# initialize the squares
+square = []
 for index in range(81):
     position, size = index2posSize(index)
     #size = sf.Vector2(75,75)
-    fyrkant.append(box(position, size, 0, 0, " "))
+    square.append(box(position, size, 0, 0, " "))
 
 textarray = []
 for index in range(81):
     text = sf.Text()
     
     text.font = font1
-    text.string = " " # str(fyrkant[index].value)
+    text.string = " " # str(square[index].value)
     text.character_size = 50
     text.color = sf.Color.WHITE
 
-    xx = fyrkant[index].position.x +20
-    yy = fyrkant[index].position.y +5
+    xx = square[index].position.x +20
+    yy = square[index].position.y +5
     position = sf.Vector2(xx,yy)
 
     text.position = position
     
     textarray.append(text)
 
+# given a mouse position give the index of the square
 def coor2index(position: sf.Vector2):
     #print(position)
     xx = ((position.x - 11) // 75)
@@ -96,6 +96,7 @@ def coor2index(position: sf.Vector2):
     #print("X:" + str(xx) + " Y:" + str(yy))
     return xx + yy * 9
 
+# It seemed a good idea in the beginning but has never been used
 def index2coor(index: int):
     yy = index // 9
     xx = index - yy * 9 
@@ -104,17 +105,15 @@ def index2coor(index: int):
     #print("X:" + str(xpos) + " Y:" + str(ypos))
     return sf.Vector2(xpos,ypos)
 
-def paintFyrkant(window, position: sf.Vector2, size: sf.Vector2, color: sf.Color):
-    
+# Paint a square at a position, size and color. Used for background and overlay colors
+def paintsquare(window, position: sf.Vector2, size: sf.Vector2, color: sf.Color):
     rectangle = sf.RectangleShape(size)
     rectangle.fill_color = color
     rectangle.position = position    
     window.draw(rectangle)
 
-
+# The sudoku grid
 def grid(window):
-
-    
     rectangle = sf.RectangleShape(sf.Vector2(679,5))
     
     xpos = 8
@@ -155,74 +154,54 @@ def grid(window):
         window.draw(rectangle)
         xpos = xpos + 75
 
-#def onsegments(window):
 def eventManager(window):
     running = True 
     global focusIndex
     global possible
-    global fyrkant
+    global square
     
     for event in window.events:
         #print(event)
         if (type(event) == sf.CloseEvent):
             print("CloseEvent")
             running = False
-    
         if (event == sf.MouseEvent):
-            pass#print("MouseEvent")
-            
-        
+            pass
         if (event == sf.MouseMoveEvent):
             index = coor2index(event.position)
             focusIndex = index
-            #print("Movement:" + str(focusIndex))
-            
         if (event == sf.MouseButtonEvent):
             if event.pressed == True:
-                #print("Button down ")# + event.button + ":" + event.position)
-                #print(event.button)
-                
                 index = coor2index(event.position)
 
-                if index == -1:
-                    pass#print("Outside")
-                else:
-                    #print("Inside")
-                    
-                    if fyrkant[index].overlay == 0:
-                        #print("Setting fyrkant[" + str(index) + "] = 1")
-                        fyrkant[index].overlay = 1
+                if index == -1: # Outside of the sudoku grid, placeholder for GUI work outside the sudoku
+                    pass 
+                else:           # Inside the sudoku grid
+                    if square[index].overlay == 0:
+                        square[index].overlay = 1
                     else:
-                        #print("Setting fyrkant[" + str(index) + "] = 0")
-                        fyrkant[index].overlay = 0
-                   
-            #else:
-                #print("Button up")
-            #print("MouseButtonEvent")
-            #print("X=" + event.y + " , Y=" + event.y)
+                        square[index].overlay = 0
+
         if (event == sf.KeyEvent):
-            pass#print("KeyEvent")
-        if (event == sf.TextEvent):
+            pass
+        if (event == sf.TextEvent):  # Keyboard interface
             c = event.unicode
             
             if (c == " " or ascii(c) >= ascii("1") and ascii(c) <= ascii("9")) and focusIndex != -1:
-                if fyrkant[focusIndex].lock == 0:
-                    fyrkant[focusIndex].value = c
+                if square[focusIndex].lock == 0:
+                    square[focusIndex].value = c
                     if possible == True:
                         updatePossible()
-                    #print(getQuadrant(focusIndex))
-                    #print(getQuadrantList(getQuadrant(focusIndex)))
-                    #print("KeyText: " + c)
             
             if c =="L" or c=="l":
-                if fyrkant[focusIndex].lock == 0:
-                    fyrkant[focusIndex].lock = 1
+                if square[focusIndex].lock == 0:
+                    square[focusIndex].lock = 1
                     print("Locked")
                 else:
-                    fyrkant[focusIndex].lock = 0
+                    square[focusIndex].lock = 0
                     print("Unlocked")
             
-            if c =="P" or c=="p":
+            if c =="P" or c=="p":     # Show the cheats, possible combinations, rudimentary right now
                 if possible == False:
                     updatePossible()
                     possible = True
@@ -231,6 +210,7 @@ def eventManager(window):
        
     return running        
 
+# Just some predefined colors
 def getColor(color: int):
 
     if color == 1:
@@ -249,39 +229,38 @@ def getColor(color: int):
         return sf.Color(255,255,255,128)
     
 
-
+# This is where we can paint translucent squares on top of the sudoku grid
 def overlaylayer(window):
     for index in range(81):
-        if fyrkant[index].overlay == 1:
+        if square[index].overlay == 1:
             #print("Index:" + str(index))
-            position = fyrkant[index].position
-            size = fyrkant[index].size
-            paintFyrkant(window, position, size, getColor(1))
+            position = square[index].position
+            size = square[index].size
+            paintsquare(window, position, size, getColor(1))
 
+# This is the rendering of the squares value 
 def textlayer(window):
     global font
     global textarray
-    global fyrkant
+    global square
     
     for index in range(81):
         text = textarray[index]
-        text.string = fyrkant[index].value
-        if fyrkant[index].lock == 1:
+        text.string = square[index].value
+        if square[index].lock == 1:
             text.color = sf.Color.BLACK
         else:
             text.color = sf.Color.WHITE
         window.draw(text)  
-    
-    
-    
-
+# rendering of the background of the square in focus, the square you are hovering over is lighted up
 def backgroundlayer(window):
     if focusIndex != -1:
-        position = fyrkant[focusIndex].position
-        size = fyrkant[focusIndex].size
-        paintFyrkant(window, position, size, getColor(4))
+        position = square[focusIndex].position
+        size = square[focusIndex].size
+        paintsquare(window, position, size, getColor(4))
     return 0
 
+# A nice big square under the whole grid
 def bottomColor(window):
     rectangle = sf.RectangleShape(sf.Vector2(679,679))
     rectangle.position = sf.Vector2(8,8)
@@ -294,33 +273,39 @@ def firstCheat(windows):
 def rulesLayer(windows):
     return 0
 
+# Get the row of the square
 def getRow(index):
     row = index // 9
     return row
 
+# Get the column of the square
 def getColumn(index):
     row = getRow(index)
     column = index - row * 9
     return column
 
+# Get the 3x3 quadrant of the square
 def getQuadrant(index):
     row = getRow(index)
     column = getColumn(index)
     quadrant = 3*(row//3) + column//3
     return quadrant
 
+# Generate a list of indexes for a particular row
 def getRowList(row):
     rowList = []
     for x in range(9):
         rowList.append(row*9 + x)
     return rowList
 
+# Generate a list of indexes for a particular column
 def getColumnList(column):
     columnList = []
     for x in range(9):
         columnList.append(column + x*9)
     return columnList
 
+# Generate a list of indexes for a particular quadrant
 def getQuadrantList(quadrant):
     quadrantList = []
     topCornerIndex = (quadrant//3)*3*9 # 0-2 => 0, 3-5 => 27, 6-8 => 54
@@ -332,18 +317,17 @@ def getQuadrantList(quadrant):
         quadrantList.append(topCornerIndex + plusIndex + x + 18)
     return quadrantList
 
-
+# get the possible numbers for a specific square, according to sudoku rules
 def getPossible(index):
-    global fyrkant
+    global square
     possibleList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     
     # Check 
-    if fyrkant[index].value != " ":
-        value = int(fyrkant[index].value)
+    if square[index].value != " ":
+        value = int(square[index].value)
         for x in range(9):
             possibleList[x] = 0
         possibleList[value - 1] = value
-        #print(possibleList)
         return possibleList
     
     
@@ -352,65 +336,43 @@ def getPossible(index):
     quadrantIndexList = getQuadrantList(getQuadrant(index))
     
     for index in rowIndexList:
-        if fyrkant[index].value != " ":
-            value = int(fyrkant[index].value)
+        if square[index].value != " ":
+            value = int(square[index].value)
             possibleList[value - 1] = 0
     for index in columnIndexList:
-        if fyrkant[index].value != " ":
-            value = int(fyrkant[index].value)
+        if square[index].value != " ":
+            value = int(square[index].value)
             possibleList[value - 1] = 0
     for index in quadrantIndexList:
-        if fyrkant[index].value != " ":
-            value = int(fyrkant[index].value)
+        if square[index].value != " ":
+            value = int(square[index].value)
             possibleList[value - 1] = 0
     
     #print(possibleList)
     return possibleList
 
-def possibleString(index):
-    global fyrkant
-    # Check 
-    if fyrkant[index].value != " ":
-        return ""
-    
-    possibleList = getPossible(index)
-    
-    text = ""
-    for x in range(9):
-        value = possibleList[x]
-        if x == 3:
-            text = text + "\n"
-        if x == 6:
-            text = text + "\n"
-        if value == 0:
-            text = text + " "
-        else:
-            text = text + str(value)
-    
-    #print(text)
-    return text
-        
+# Update each square with the possible numbers        
 def updatePossible():
-    global fyrkant
+    global square
     
     for index in range(81):
-        fyrkant[index].possible = getPossible(index)
+        square[index].possible = getPossible(index)
         
-        
+# And now render the possible number layer        
 def possibleLayer(window):
-    global fyrkant
+    global square
     
     if possible == False:
         return 0
     
     for index in range(81):
         for x in range(9):
-            if fyrkant[index].value == " ":
-                possibleList = fyrkant[index].possible
+            if square[index].value == " ":
+                possibleList = square[index].possible
                 value = possibleList[x]
                 if value != 0:
-                    xx = fyrkant[index].position.x
-                    yy = fyrkant[index].position.y
+                    xx = square[index].position.x
+                    yy = square[index].position.y
                     xxx = (x%3) * 23 + 5
                     yyy = (x//3) * 22 + 1
                     
@@ -426,34 +388,17 @@ def possibleLayer(window):
 # Create the main window
 window = sf.RenderWindow(sf.VideoMode(1024, 768), "PySFML test")
 window.vertical_synchronization = True
-# Create a graphical string to display
-
-
-
-#for index in textarray:
-#    window.draw(textarray[index])
-
-text1 = sf.Text()
-text1.font = font1
-text1.string = "AAAA\nAAAA"
-text1.character_size = 100
-text1.color = sf.Color(255,255,255,100)
-text1.position = sf.Vector2(700,200)
-text1.style = sf.Text.UNDERLINED
 
 
 # Start the game loop
 running = True
-xsegment = 0
-ysegment = 0
 
+# we do not want to see possible number at startup
 possible = False
 
 while running:
-    #event = sf.Event()
-    running = eventManager(window)
-    
 
+    running = eventManager(window)
 
     # Clear screen, draw the text, and update the window
     window.clear()
@@ -470,19 +415,8 @@ while running:
     textlayer(window)
     
     overlaylayer(window)
-    
-#    for text in textarray:
-#        window.draw(text) 
-    
-    # window.draw(text1)
-    # text1.rotate(90)
-    # window.draw(text1)
-    # text1.rotate(-90)
-    #
 
     window.display()
-    
-    #print("*")
 
 if __name__ == '__main__':
     pass
